c03ffa6b865dfd7af0d0efe43053d1f56df6fcb2
adan <adanalin113@gmail.com>
2021-09-14 17:02:09 -0400

    commit tree f0c583fa084eb4d82e11fbb023eb2b4455fcd0e0

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function didWin(element){
+    return (element.result === 'W'); 
+}
+
+
+function superbowlWin(arry){
+    const result = arry.find(didWin);
+    if(result !== undefined){
+        return result.year
+    } else {
+        return result
+    }
+}
+
 
